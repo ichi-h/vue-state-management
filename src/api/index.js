7082baa6e5d38585297ce278c4ce6efce6cde074
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-01-30 10:24:52 +0900

    HTTP Request群の追加

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// apiを用途ごとにまとめる
+// 各メソッドはHTTP Requestの送出/HTTP Responseの返却を担当
+// データ加工等のその他の処理は別メソッドに譲る
+export const CounterService = {
+  fetchCount: () => {
+    return new Promise((resolve) => {
+      axios.get("/api/fetch_count")
+        .then((res) => resolve(res.data));
+    });
+  },
+  patchCount: async (newCount) => {
+    await axios.patch("/api/patch_count", { count: newCount });
+  },
+};
