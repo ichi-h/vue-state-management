643cb9ae7ba149ec3a5c4ed751f5658834eafa67
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-01-30 12:42:27 +0900

    ルーティングの追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import Home from '../templates/Home.vue'
+import Counter from "../templates/Counter/Index.vue";
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../templates/About.vue')
+  },
+  {
+    path: '/counter',
+    name: 'Counter',
+    component: Counter
   }
 ]
 
