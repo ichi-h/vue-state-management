8e7ab8711da56c93a3395545760dc7ad36f56d29
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-01-30 12:43:51 +0900

    汎用的に使用できるコンポーネントの追加

---
+++ src/components/uiParts/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="onClick">{{ value }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+  },
+  methods: {
+    onClick: function (e) {
+      this.$emit("click", e);
+    },
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/uiParts/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    <slot />
+  </h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: large;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
