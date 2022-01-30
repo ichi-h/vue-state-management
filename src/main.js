78d26788fa1031a41b33b606f1a60b7faad480ef
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-01-30 12:42:00 +0900

    モックサーバーを使えるように修正

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 Vue.config.productionTip = false
 
 new Vue({
