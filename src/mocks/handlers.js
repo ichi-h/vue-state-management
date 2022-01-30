1dd238cdf50c60af7eaf8b5c2d77421d76dc94d7
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-01-30 09:18:56 +0900

    mswの追加

---
+++ src/mocks/handlers.js
@@ -0,0 +1,18 @@
+import { rest } from "msw";
+
+const sleep = (ms) => {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve(), ms, false)
+  );
+};
+
+export const handlers = [
+  rest.get("/api/fetch_count", async (_, res, ctx) => {
+    await sleep(2000);
+    return res(ctx.status(200), ctx.json({ count: 0 }));
+  }),
+  rest.patch("/api/patch_count", async (_, res, ctx) => {
+    await sleep(2000);
+    return res(ctx.status(200));
+  }),
+];
