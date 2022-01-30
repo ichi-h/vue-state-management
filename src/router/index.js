b99737b6b26bc8fc7911bd251b61d1c55f7aaed8
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-01-30 13:12:20 +0900

    ディレクトリの修正

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from '../templates/Home.vue'
-import Counter from "../templates/Counter/Index.vue";
+import Home from '../pages/Home.vue'
+import Counter from "../pages/Counter/Index.vue";
 
 Vue.use(VueRouter)
 
@@ -18,7 +18,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../templates/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../pages/About.vue')
   },
   {
     path: '/counter',
